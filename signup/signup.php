4686f07f40195e554d35792327b46ae0d478eb51
Anh Mai <hoanganh.hoang44@gmail.com>
2020-02-25 19:24:45 +0000

    Updated signup.php and HoangAnh review

--- signup/signup.php
+++ signup/signup.php
@@ -1,73 +1,73 @@
 <?php
-if (session_status() == PHP_SESSION_NONE) {
-    session_start();
-}
+                    if (session_status() == PHP_SESSION_NONE) {
+                        session_start();
+                    }
 
-include("setup.php");
-$conn_id = setup_connect()
-    or die("cannot connect to server");
-//if (isset($_POST['submit'])) {
+                    include("setup.php");
+                    $conn_id = setup_connect()
+                        or die("cannot connect to server");
+                    //if (isset($_POST['submit'])) {
 
-$fname = $_POST['fname'];
-$lname = $_POST['lname'];
-$pword = $_POST['pword'];
-$pword1 = $_POST['pword1'];
-$uni = $_POST['uni'];
-$accType = $_POST['accType'];
-$email = $_POST['email'];
+                    $fname = $_POST['fname'];
+                    $lname = $_POST['lname'];
+                    $pword = $_POST['pword'];
+                    $pword1 = $_POST['pword1'];
+                    $uni = $_POST['uni'];
+                    $accType = $_POST['accType'];
+                    $email = $_POST['email'];
 
-$uppercase = preg_match('@[A-Z]@', $pword);
-$lowercase = preg_match('@[a-z]@', $pword);
-$number    = preg_match('@[0-9]@', $pword);
+                    $uppercase = preg_match('@[A-Z]@', $pword);
+                    $lowercase = preg_match('@[a-z]@', $pword);
+                    $number    = preg_match('@[0-9]@', $pword);
 
-$output = "";
+                    $output = "";
 
-$pwordlen = strlen($pword);
-//Check if the password is valid
-if (!$uppercase || !$lowercase || !$number || $pwordlen < 8 || $pwordlen > 16) {
-    $output .= "<p>Invalid Password: Passwords must be between 8 and 16 characters long and contain an uppercase letter, lowercase letter, and a digit</p>";
-}
+                    $pwordlen = strlen($pword);
+                    //Check if the password is valid
+                    if (!$uppercase || !$lowercase || !$number || $pwordlen < 8 || $pwordlen > 16) {
+                        $output .= "<p>Invalid Password: Passwords must be between 8 and 16 characters long and contain an uppercase letter, lowercase letter, and a digit</p>";
+                    }
 
-//Check if the passwords match
-if ($pword != $pword1) {
-    $output .= "<p>Error: Passwords do not match</p>";
-}
+                    //Check if the passwords match
+                    if ($pword != $pword1) {
+                        $output .= "<p>Error: Passwords do not match</p>";
+                    }
 
-//Check if the email is valid
-if ($uni == "University College Cork") {
-    if ($accType == "student") {
-        $studentEmailUCC = preg_match('/^[0-9]{1,9}@umail.ucc.ie$/', $email);
-        if (!$studentEmailUCC) {
-            $output .= "<p>Invalid student E-mail for University College Cork</p>";
-        }
-    } else {
-        $tutorEmailUCC = preg_match('/^[0-9A-Za-z]{1,20}[@](?:[0-9A-Za-z]{1,20}.ucc.ie|ucc.ie)$/', $email);
-        if (!$tutorEmailUCC) {
-            $output .= "<p>Invalid staff E-mail for University College Cork</p>";
-        }
-    }
-}
+                    //Check if the email is valid
+                    if ($uni == "University College Cork") {
+                        if ($accType == "student") {
+                            $studentEmailUCC = preg_match('/^[0-9]{1,9}@umail.ucc.ie$/', $email);
+                            if (!$studentEmailUCC) {
+                                $output .= "<p>Invalid student E-mail for University College Cork</p>";
+                            }
+                        } else {
+                            $tutorEmailUCC = preg_match('/^[0-9A-Za-z]{1,20}[@](?:[0-9A-Za-z]{1,20}.ucc.ie|ucc.ie)$/', $email);
+                            if (!$tutorEmailUCC) {
+                                $output .= "<p>Invalid staff E-mail for University College Cork</p>";
+                            }
+                        }
+                    }
 
-if ($output == "") {
-    $mysqli_stmt = $conn_id->prepare("INSERT INTO users(fname, lname, pword, accType, uni, email) VALUES (?, ?, ?, ?, ?, ?);");
-    $hashpword = hash('sha224', $pword);
-    $mysqli_stmt->bind_param("ssssss", $fname, $lname, $hashpword, $accType, $uni, $email);
-    mysqli_stmt_execute($mysqli_stmt);
-    $output .= "<p>Account creation successful</p>";
-    $_SESSION["college"] = $uni;
-    $_SESSION["user"] = $email;
-    $_SESSION["pword"] = $hashpword;
-    $_SESSION["accType"] = $accType;
-    if ($accType == "tutor") {
-        header("Location: ../tutor-upload.php");
-    } else if ($accType == "student") {
-        header("Location: ../courses.php");
-    } else {
-        echo "fail";
-    }
-}
+                    if ($output == "") {
+                        $mysqli_stmt = $conn_id->prepare("INSERT INTO users(fname, lname, pword, accType, uni, email) VALUES (?, ?, ?, ?, ?, ?);");
+                        $hashpword = hash('sha224', $pword);
+                        $mysqli_stmt->bind_param("ssssss", $fname, $lname, $hashpword, $accType, $uni, $email);
+                        mysqli_stmt_execute($mysqli_stmt);
+                        $output .= "<p>Account creation successful</p>";
+                        $_SESSION["college"] = $uni;
+                        $_SESSION["user"] = $email;
+                        $_SESSION["pword"] = $hashpword;
+                        $_SESSION["accType"] = $accType;
+                        if ($accType == "tutor") {
+                            header("Location: ../tutor-upload.php");
+                        } else if ($accType == "student") {
+                            header("Location: ../courses.php");
+                        } else {
+                            echo "fail";
+                        }
+                    }
 
-?>
+                    ?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -128,10 +128,6 @@ if ($output == "") {
                             </div>
                         </div>
 
-
-
-
-
                         <div class="form-row">
                             <div class="name">University</div>
                             <div class="value">
@@ -148,18 +144,6 @@ if ($output == "") {
                                 </div>
                             </div>
                         </div>
-
-
-
-
-
-
-
-
-
-
-
-
 
 
                         <div class="form-row">
@@ -189,9 +173,6 @@ if ($output == "") {
                             </div>
                         </div>
 
-
-
-
                         <div class="form-row p-t-20">
                             <label class="label label--block">I am a:</label>
                             <div class="p-t-15">
@@ -205,10 +186,6 @@ if ($output == "") {
                                 </label>
                             </div>
                         </div>
-
-
-
-
 
                         <div>
                             <button class="btn btn--radius-2 btn--red" type="submit">Register</button>
