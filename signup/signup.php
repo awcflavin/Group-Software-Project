04810651b00178afa6b3cce756664190c4eab372
Anh Mai <hoanganh.hoang44@gmail.com>
2020-02-28 18:12:42 +0000

    Add home button in signup so user can go back

--- signup/signup.php
+++ signup/signup.php
@@ -30,7 +30,7 @@ if (!$uppercase || !$lowercase || !$number || $pwordlen < 8 || $pwordlen > 16) {
 
 //Check if the passwords match
 if ($pword != $pword1) {
-    $output .= "<p>Error: Passwords do not match</p>";
+    $output .= "<p>Passwords do not match</p>";
 }
 
 //Check if the email is valid
@@ -66,170 +66,186 @@ if ($output == "") {
         echo "fail";
     }
 }
-
 ?>
-<<!DOCTYPE html>
-    <html lang="en">
+<!DOCTYPE html>
+<html lang="en">
 
-    <head>
-        <!-- Required meta tags-->
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <meta name="description" content="Colorlib Templates">
-        <meta name="author" content="Colorlib">
-        <meta name="keywords" content="Colorlib Templates">
+<head>
+    <!-- Required meta tags-->
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
-        <!-- Title Page-->
-        <title>Au Register Forms by Colorlib</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Colorlib Templates">
+    <meta name="author" content="Colorlib">
+    <meta name="keywords" content="Colorlib Templates">
+    <link rel="stylesheet" href="css/home_button.css" />
+    <nav class="navbar navbar-expand-lg navbar-light">
 
-        <!-- Icons font CSS-->
-        <link href="vendor/mdi-font/css/material-design-iconic-font.min.css" rel="stylesheet" media="all">
-        <link href="vendor/font-awesome-4.7/css/font-awesome.min.css" rel="stylesheet" media="all">
-        <!-- Font special for pages-->
-        <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i" rel="stylesheet">
+        <div class="container">
+            <div class="collapse navbar-collapse offset" id="navbarSupportedContent">
+                <ul class="nav navbar-nav menu_nav ml-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="../index.html"><i class="fa fa-home"></i> Home</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
 
-        <!-- Vendor CSS-->
-        <link href="vendor/select2/select2.min.css" rel="stylesheet" media="all">
-        <link href="vendor/datepicker/daterangepicker.css" rel="stylesheet" media="all">
+    <!-- Title Page-->
+    <title>Au Register Forms by Colorlib</title>
 
-        <!-- Main CSS-->
-        <link href="css/main.css" rel="stylesheet" media="all">
-    </head>
+    <!-- Icons font CSS-->
+    <link href="vendor/mdi-font/css/material-design-iconic-font.min.css" rel="stylesheet" media="all">
+    <link href="vendor/font-awesome-4.7/css/font-awesome.min.css" rel="stylesheet" media="all">
+    <!-- Font special for pages-->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i"
+        rel="stylesheet">
 
-    <body>
-        <div class="page-wrapper bg-gra-03 p-t-45 p-b-50">
-            <div class="wrapper wrapper--w790">
-                <div class="card card-5">
-                    <div class="card-heading">
-                        <h2 class="title">Event Registration Form</h2>
-                    </div>
-                    <div class="card-body">
-                        <?php
-                        echo $output;
-                        ?>
-                        <form action="signup.php" method="POST">
-                            <div class="form-row m-b-55">
-                                <div class="name">Name</div>
-                                <div class="value">
-                                    <div class="row row-space">
-                                        <div class="col-2">
-                                            <div class="input-group-desc">
-                                                <input class="input--style-5" type="text" name="fname" required>
-                                                <label class="label--desc">first name</label>
-                                            </div>
-                                        </div>
-                                        <div class="col-2">
-                                            <div class="input-group-desc">
-                                                <input class="input--style-5" type="text" name="lname" required>
-                                                <label class="label--desc">last name</label>
-                                            </div>
+    <!-- Vendor CSS-->
+    <link href="vendor/select2/select2.min.css" rel="stylesheet" media="all">
+    <link href="vendor/datepicker/daterangepicker.css" rel="stylesheet" media="all">
+
+    <!-- Main CSS-->
+    <link href="css/main.css" rel="stylesheet" media="all">
+</head>
+
+<body>
+    <div class="page-wrapper bg-gra-03 p-t-45 p-b-50">
+        <div class="wrapper wrapper--w790">
+            <div class="card card-5">
+                <div class="card-heading">
+                    <h2 class="title">Event Registration Form</h2>
+                </div>
+                <div class="card-body">
+                    <?php
+                        echo  $output ;
+                    ?>
+                    <form action="signup.php" method="POST">
+                        <div class="form-row m-b-55">
+                            <div class="name">Name</div>
+                            <div class="value">
+                                <div class="row row-space">
+                                    <div class="col-2">
+                                        <div class="input-group-desc">
+                                            <input class="input--style-5" type="text" name="fname" required>
+                                            <label class="label--desc">first name</label>
                                         </div>
                                     </div>
-                                </div>
-                            </div>
-
-
-
-
-
-                            <div class="form-row">
-                                <div class="name">University</div>
-                                <div class="value">
-                                    <div class="input-group">
-                                        <div class="rs-select2 js-select-simple select--no-search">
-                                            <select name="uni" required>
-                                                <option disabled="disabled" selected="selected">Choose option</option>
-                                                <option>University College Cork</option>
-                                                <option>University College Limerick</option>
-                                                <option>University College Galway</option>
-                                            </select>
-                                            <div class="select-dropdown"></div>
+                                    <div class="col-2">
+                                        <div class="input-group-desc">
+                                            <input class="input--style-5" type="text" name="lname" required>
+                                            <label class="label--desc">last name</label>
                                         </div>
                                     </div>
                                 </div>
                             </div>
+                        </div>
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-                            <div class="form-row">
-                                <div class="name">Email</div>
-                                <div class="value">
-                                    <div class="input-group">
-                                        <input class="input--style-5" type="email" name="email" required>
+                        <div class="form-row">
+                            <div class="name">University</div>
+                            <div class="value">
+                                <div class="input-group">
+                                    <div class="rs-select2 js-select-simple select--no-search">
+                                        <select name="uni" required>
+                                            <option disabled="disabled" selected="selected">Choose option</option>
+                                            <option>University College Cork</option>
+                                            <option>University College Limerick</option>
+                                            <option>University College Galway</option>
+                                        </select>
+                                        <div class="select-dropdown"></div>
                                     </div>
                                 </div>
                             </div>
+                        </div>
 
-                            <div class="form-row">
-                                <div class="name">Password</div>
-                                <div class="value">
-                                    <div class="input-group">
-                                        <input class="input--style-5" type="Password" name="pword" required>
-                                    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+                        <div class="form-row">
+                            <div class="name">Email</div>
+                            <div class="value">
+                                <div class="input-group">
+                                    <input class="input--style-5" type="email" name="email" required>
                                 </div>
                             </div>
+                        </div>
 
-                            <div class="form-row">
-                                <div class="name">Confirm Password</div>
-                                <div class="value">
-                                    <div class="input-group">
-                                        <input class="input--style-5" type="Password" name="pword1" required>
-                                    </div>
+                        <div class="form-row">
+                            <div class="name">Password</div>
+                            <div class="value">
+                                <div class="input-group">
+                                    <input class="input--style-5" type="Password" name="pword" required>
                                 </div>
                             </div>
+                        </div>
 
-
-
-
-                            <div class="form-row p-t-20">
-                                <label class="label label--block">I am a:</label>
-                                <div class="p-t-15">
-                                    <label class="radio-container m-r-55">Student
-                                        <input value="student" type="radio" checked="checked" name="accType">
-                                        <span class="checkmark"></span>
-                                    </label>
-                                    <label class="radio-container">Tutor
-                                        <input value="tutor" type="radio" name="accType">
-                                        <span class="checkmark"></span>
-                                    </label>
+                        <div class="form-row">
+                            <div class="name">Confirm Password</div>
+                            <div class="value">
+                                <div class="input-group">
+                                    <input class="input--style-5" type="Password" name="pword1" required>
                                 </div>
                             </div>
+                        </div>
 
 
 
 
-
-                            <div>
-                                <button class="btn btn--radius-2 btn--red" type="submit">Register</button>
+                        <div class="form-row p-t-20">
+                            <label class="label label--block">I am a:</label>
+                            <div class="p-t-15">
+                                <label class="radio-container m-r-55">Student
+                                    <input value="student" type="radio" checked="checked" name="accType">
+                                    <span class="checkmark"></span>
+                                </label>
+                                <label class="radio-container">Tutor
+                                    <input value="tutor" type="radio" name="accType">
+                                    <span class="checkmark"></span>
+                                </label>
                             </div>
-                        </form>
-                    </div>
+                        </div>
+
+
+
+
+
+                        <div>
+                            <button class="btn btn--radius-2 btn--red" type="submit">Register</button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
+    </div>
 
-        <!-- Jquery JS-->
-        <script src="vendor/jquery/jquery.min.js"></script>
-        <!-- Vendor JS-->
-        <script src="vendor/select2/select2.min.js"></script>
-        <script src="vendor/datepicker/moment.min.js"></script>
-        <script src="vendor/datepicker/daterangepicker.js"></script>
+    <!-- Jquery JS-->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <!-- Vendor JS-->
+    <script src="vendor/select2/select2.min.js"></script>
+    <script src="vendor/datepicker/moment.min.js"></script>
+    <script src="vendor/datepicker/daterangepicker.js"></script>
 
-        <!-- Main JS-->
-        <script src="js/global.js"></script>
+    <!-- Main JS-->
+    <script src="js/global.js"></script>
 
-    </body><!-- This templates was made by Colorlib (https://colorlib.com) -->
+</body><!-- This templates was made by Colorlib (https://colorlib.com) -->
 
-    </html>
-    <!-- end document-->
+</html>
+<!-- end document-->
