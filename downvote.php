ab5f02e79f6d9f5dc73aadd5bfccab469784346e
awcflavin <55809901+awcflavin@users.noreply.github.com>
2020-03-03 18:52:47 +0000

    view paper page including answers and voting

---
+++ downvote.php
@@ -0,0 +1,36 @@
+<?php
+include "setup.php";
+include "security.php";
+
+$conn_id = setup_connect()
+	or die ("cannot connect to server");
+
+$paperID = $_GET['answerID'];
+$userID=$_GET['userID'];
+$voted=$_GET['voted'];
+
+if ($voted = "no") {
+	
+	$query = $conn_id->prepare("INSERT INTO votes (userID, answerID, vote) VALUES ($userID, $answerID, 1);");
+	$query->bind_param("ss", $userID, $answerID);
+	$query->execute()
+		or die("Cannot execute query");
+
+	$query = $conn_id->prepare("UPDATE answers SET votes = votes - 1 WHERE answerID = ?;");
+	$query->bind_param("s", $answerID);
+	$query->execute()
+		or die("Cannot execute query");
+
+} elseif ($voted = "up") {
+	$query = $conn_id->prepare("UPDATE votes (vote) SET vote = -1 WHERE userID = $userID AND answerID = $answerID);");
+	$query->bind_param("ss", $userID, $answerID);
+	$query->execute()
+		or die("Cannot execute query");
+	
+	$query = $conn_id->prepare("UPDATE answers SET votes = votes - 1 WHERE answerID = ?;");
+	$query->bind_param("s", $answerID);
+	$query->execute()
+		or die("Cannot execute query");
+}
+
+?>
---
+++ view.php
@@ -0,0 +1,137 @@
+<?php
+include "setup.php";
+//include "security.php";
+
+$conn_id = setup_connect()
+	or die ("cannot connect to server");
+
+$userID = $_SESSION['id']
+$paperID = $_GET['id'];
+$uni=$_GET['uni'];
+$module =$_GET['module'];
+$year=$_GET['year'];
+$season=$_GET['season'];
+
+$path = "papers/$uni/$module/$year/$season/$paperID.pdf";
+
+$answers = "<section id='answers'>
+				<iframe style='display: none;' name='target'></iframe>
+				<form action='view.php?ID=$paperID&uni=$uni&module=$module&year=$year&season=$season' method='post'>
+					<select id='selectQ' name='Question'>
+						<option value=1 selected>1</option>";
+
+// Query the papers table to check the number of questions in this paper and populates a drop down list based on this number
+$query = $conn_id->prepare("SELECT questions FROM papers WHERE id = ?;");
+$query->bind_param("s", $paperID);
+$query->execute()
+	or die("Cannot execute query");
+$qs=$query->get_result();
+
+while ($row = mysqli_fetch_assoc($qs)) {
+	$noQ = $row['questions'];
+	for ($i = 2; $i <= $noQ; $i++) {
+		$answers .=
+						"<option value='$i'>$i</option>";
+	}
+}
+
+$answers .=
+					"</select>
+				</form>";
+
+if (isset($_POST['question'])) {
+	$question = $_POST['question'];
+} else {
+	$question = 1;
+}
+
+$query = $conn_id->prepare("SELECT * FROM answers WHERE id = ? AND paperID = ? ORDER BY votes;");
+$query->bind_param("ss", $paperID, $question);
+$query->execute()
+	or die("Cannot execute query");
+$result = $query->get_result();
+
+while ($row = mysqli_fetch_assoc($result)) {
+	$answerID = $row['id'];
+	$posterID = $row['userID'];
+	$votes = $row['votes'];
+	$timestamp =$row['postTime'];
+	$answer=$row['answer'];
+	
+	$query1 = "SELECT fname, lname, accType FROM users WHERE ID = $posterID;";
+	$result1 = mysqli_query($conn_id, $query1)
+		or die("Cannot execute query");
+	
+	while ($row = mysqli_fetch_assoc($result1)) {
+		$fname=$row['fname'];
+		$lname=$row['lname'];
+		$accType=$row['accType'];
+	}
+	
+	// Queries the votes database to check if this user has already voted on this answer
+	$query2 = $conn_id->prepare("SELECT vote FROM votes WHERE userID=? AND answerID=?;");
+	$no1 = 1;
+	$query2->bind_param("ss", $no1, $no1);
+	$query2->execute()
+		or die("Cannot execute query");
+	$result2 = $query2->get_result();
+
+	// If the user has voted on this answer, upvoted is set to up if they have upvoted, down if downvoted, no if didnt vote
+	if (mysqli_num_rows($result2)>0) {
+		$row = mysqli_fetch_assoc($result2);
+		$voteValue=$row['vote'];
+		if ($voteValue >1) {
+			$voted="up";
+		} else {
+			$voted="down";
+		}
+	} else {
+		$voted="noVote";
+	}
+	
+	// Checks the account type of the person who submitted the answer to highlight tutor answers
+	if ($accType=='tutor') {
+		$details="tutorDetails";
+	} else {
+		$details="studentDetails";
+	}
+
+	$answers .=
+		"<div class='submission'>
+			<div class='$details'>
+				<p class='fname'>$fname</p>
+				<p class='lname'>$lname</p>
+				<p class='timestamp'>$timestamp</p>
+			</div>
+			<div class='voting $voted'>
+				<a href='upvote.php/?answerID=$answerID&userID=$userID&voted=$voted' class='upvote' target='target'><img src='/icons/upvote.svg'></a
+				<p class='votes' id='$answerID'>$votes</p>
+				<a href='downvote.php/?answerID=$answerID&userID=$userID&voted=$voted' class='downvote' target='target'><img src='/icons/downvote.svg'></a>
+			</div>
+			<div class='answer'>
+				<p>$answer</p>
+			</div>
+		 </div>";
+}
+
+$answers .= "</section>";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<!-- Required meta tags -->
+	<meta charset="utf-8" />
+	<meta
+    	name="viewport"
+    	content="width=device-width, initial-scale=1, shrink-to-fit=no"
+	/>
+	<link rel="icon" href="img/favicon.png" type="image/png" />
+	<link rel="stylesheet" type="text/css" href="styles.css">
+	<script src="voting.js"></script>
+	<title>Exam Paper View</title>
+</head>
+<body>
+	<iframe id='paper' src="<?php echo $path; ?>" width=900 height=900></iframe>
+	<?php echo $answers; ?>
+</body>
